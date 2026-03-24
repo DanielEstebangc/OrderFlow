f9658cce6db4336dd2ff344de9a8d701164d3912
Daniel Galvis <danielgalvis.3107@gmail.com>
2026-03-24 13:58:02 -0500

    implementacion de libreria para iconos de google
    implementacion de icono para el componente header

--- src/components/layout/Header/header.jsx
+++ src/components/layout/Header/header.jsx
@@ -1,11 +1,12 @@
 import "./header.css";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header-container">
 
-        <span className="header-icon">icono</span>
+        <span className="header-icon"><ShoppingCartIcon color="white" sx={{ fontSize: 50 }} /></span>
 
         <div className="header-text">
           <h1 className="header-title">Formulario de Pedido</h1>
